25271882ba65b7af55f99f36b9bffe4501d1030f
bhathiya-wimalasinghe <bhathiyawimalasinghe@gmail.com>
2024-03-11 17:40:11 +0530

    insert title and instructions to the game

--- src/App.js
+++ src/App.js
@@ -52,6 +52,11 @@ export default function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="die-container">{dieElements}</div>
       <button className="btn-roll-dice" onClick={rollDice}>
         Roll
